455ff24ecf02bc58bbd48009198883df9e5389b1
bigassopper <61640594+bigassopper@users.noreply.github.com>
2020-06-17 03:31:03 -0300

    Added a new enemy

---
+++ Assets/penguim.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class penguim : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public int jumpSpeed;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * jumpSpeed);
+    }
+
+}
---
+++ Assets/penguimSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class penguimSpawner : MonoBehaviour
+{
+    public GameObject penguim;
+    public Transform[] spots;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Instantiate(penguim, spots[0].position, spots[0].rotation);
+            Instantiate(penguim, spots[1].position, spots[1].rotation);
+            Destroy(gameObject);
+
+        } 
+    }
+}
