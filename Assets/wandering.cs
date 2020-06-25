abd76a3f209a09b02b9894e5d8889fe4b728968b
bigassopper <61640594+bigassopper@users.noreply.github.com>
2020-06-25 00:09:26 -0300

    penguin and wandering system

---
+++ Assets/wandering.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wandering : MonoBehaviour
+{
+    public int speed = 20;
+
+    private float wanderTimer = 0;
+    public float endtime, finishtime;
+
+    public void FixedUpdate()
+    {
+        wanderTimer += Time.deltaTime;
+
+        if (wanderTimer > 0 && wanderTimer < endtime)
+        {
+            transform.position += transform.TransformDirection(Vector2.right) * speed * Time.deltaTime;
+        }
+        else
+        {
+            transform.position += transform.TransformDirection(Vector2.left) * speed * Time.deltaTime;
+        }
+
+        if(wanderTimer >= finishtime)
+        {
+            wanderTimer = 0;
+        }
+    }
+
+}
